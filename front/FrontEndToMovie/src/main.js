2ef0fd87d336f95079696ea053ccbff3330d63d5
Ekaterina Mikhailova <19kotik98@gmail.com>
2019-03-19 10:50:47 +0300

    Добавление проекта vue-cli и частичный перевод страниц на систему сборки

---
+++ front/FrontEndToMovie/src/main.js
@@ -0,0 +1,27 @@
+import '@babel/polyfill'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import routes from './routes'
+Vue.config.productionTip = false
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      const matchingView = routes[this.currentRoute]
+      return matchingView
+        ? require('./pages/' + matchingView + '.vue')
+        : require('./pages/Home.vue')
+    }
+  },
+  render (h) {
+    return h(this.ViewComponent)
+  }
+})
+
+window.addEventListener('popstate', () => {
+  app.currentRoute = window.location.pathname
+})
