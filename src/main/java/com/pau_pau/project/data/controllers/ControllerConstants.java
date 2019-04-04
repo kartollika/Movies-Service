9ebb4a126377c0242c7543f1d3b0e5ede75497fd
Dmitrii Maksimov <maksimow.dmitrij@yandex.ru>
2019-04-04 22:29:22 +0300

    refactor films controller and service

--- src/main/java/com/pau_pau/project/data/controllers/ControllerConstants.java
+++ src/main/java/com/pau_pau/project/data/controllers/ControllerConstants.java
@@ -2,7 +2,7 @@ package com.pau_pau.project.data.controllers;
 
 public class ControllerConstants {
     public static final String FILMS_URL = "/api/films";
-    public static final String FILM_BY_ID = "/film";
+    public static final String FILM_BY_ID = "/film/{id}";
 
 
     public static final String DIRECTORS_URL = "/api/directors";
