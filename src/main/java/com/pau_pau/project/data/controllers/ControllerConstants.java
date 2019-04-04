5164816180503ac727d9995f1f11c7ee63a1cf08
Dmitrii Maksimov <maksimow.dmitrij@yandex.ru>
2019-04-04 22:38:59 +0300

    refactor directors controller and service

--- src/main/java/com/pau_pau/project/data/controllers/ControllerConstants.java
+++ src/main/java/com/pau_pau/project/data/controllers/ControllerConstants.java
@@ -6,7 +6,9 @@ public class ControllerConstants {
 
 
     public static final String DIRECTORS_URL = "/api/directors";
-    public static final String DIRECTOR_BY_ID = "/director";
+    public static final String DIRECTOR_PATH_ID = "id";
+    public static final String DIRECTOR_BY_ID = "/director/{" + DIRECTOR_PATH_ID + "}";
+
 
     public static final String ACCOUNT_URL = "/api/account";
     public static final String ACCOUNT_USERNAME = "/{username}";
