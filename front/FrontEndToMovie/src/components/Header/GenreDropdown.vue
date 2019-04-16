a75be1fe1edf89b4f2e8305c9af5e2727fcc1983
sergejmurskij <sm2198sm@gmail.com>
2019-04-16 16:49:25 +0300

    wishlist and admin-panel second-part

---
+++ front/FrontEndToMovie/src/components/Header/GenreDropdown.vue
@@ -0,0 +1,24 @@
+<template>
+  <base-dropdown>
+    <a slot="title" class="dropdown-toggle nav-link nav-link-icon">Жанры</a>
+    <a class="dropdown-item " href="/genre/экшн/">Экшн</a>
+    <a class="dropdown-item" href="/genre/аниме/">Аниме</a>
+    <a class="dropdown-item" href="/genre/комедии/">Комедии</a>
+    <a class="dropdown-item" href="/genre/ужасы/">Ужасы</a>
+    <a class="dropdown-item" href="/genre/фантастика/">Фантастика</a>
+    <a class="dropdown-item" href="/genre/фентези/">Фэнтэзи</a>
+  </base-dropdown>
+</template>
+
+<script>
+import BaseDropdown from "../Base/BaseDropdown.vue"
+export default {
+  components: {
+    BaseDropdown
+  }
+}
+</script>
+
+<style>
+
+</style>
