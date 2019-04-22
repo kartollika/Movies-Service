c3d467336dc097b0c3902bff2cf9c5b85ccbd9b6
MikeKoms <ponilol37@gmail.com>
2019-04-22 22:38:09 +0300

    init commit

---
+++ src/main/java/com/pau_pau/omdb_client/OmdbClient.java
@@ -0,0 +1,4 @@
+package com.pau_pau.omdb_client;
+
+public class OmdbClient {
+}
---
+++ src/main/java/com/pau_pau/omdb_client/OmdbClientApplication.java
@@ -0,0 +1,13 @@
+package com.pau_pau.omdb_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmdbClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmdbClientApplication.class, args);
+	}
+
+}
