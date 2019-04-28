cbc482acd474b71d5256c22ea6e4551ca2d6296e
Dmitrii Maksimov <maksimow.dmitrij@yandex.ru>
2019-04-28 12:18:44 +0300

    Temporary removed Commentable for modified state

--- src/main/java/com/pau_pau/project/models/states/concretes/ModifiedFilmState.java
+++ src/main/java/com/pau_pau/project/models/states/concretes/ModifiedFilmState.java
@@ -2,31 +2,21 @@ package com.pau_pau.project.models.states.concretes;
 
 import com.pau_pau.project.models.accounts.Account;
 import com.pau_pau.project.models.accounts.Role;
-import com.pau_pau.project.models.states.Commentable;
 import com.pau_pau.project.models.states.FilmState;
 import com.pau_pau.project.models.states.FilmStatus;
 
-import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("modified")
-public class ModifiedFilmState extends FilmState implements Commentable {
-
-    @Column
-    String comment = null;
+public class ModifiedFilmState extends FilmState {
 
     public ModifiedFilmState() {
     }
 
     public ModifiedFilmState(FilmState oldState) {
         super(oldState);
-    }
-
-    public ModifiedFilmState(FilmState oldState, String comment) {
-        super(oldState);
-        this.comment = comment;
     }
 
     @Override
@@ -48,13 +38,4 @@ public class ModifiedFilmState extends FilmState implements Commentable {
         return FilmStatus.MODIFIED;
     }
 
-    @Override
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public String getComment() {
-        return comment;
-    }
 }
